cb60f7d56723ff3d6f964ded660ee261d461b62f
jakoko <aerisgurlie777@gmail.com>
2015-02-10 23:50:46 -0800

    created worker that makes API call for tweets

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -6,7 +6,13 @@ class StaticPagesController < ApplicationController
     @test = request.env['omniauth.auth']
 
     if request.env['omniauth.auth'] != nil && params[:provider] == 'twitter'
+      # For testing purpose. Delete when workers are working
       @tweets = get_tweets(request.env['omniauth.auth'])
+
+      # Workers
+      user_hash = build_hash(request.env['omniauth.auth'])
+      TwitterBuildTweetText.perform_async(user_hash)
+      # Add code to start superworker
     end
   end
 end
