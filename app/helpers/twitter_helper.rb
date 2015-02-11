0c65f956f186274cb372aae8f21173a03d0b07c8
jakoko <aerisgurlie777@gmail.com>
2015-02-10 22:52:57 -0800

    added to twitter helper file (methods to use with workers)

--- app/helpers/twitter_helper.rb
+++ app/helpers/twitter_helper.rb
@@ -1,8 +1,8 @@
 module TwitterHelper
 
-  # Check out this website. It helped me sort of understand OAuth and what methods to use
-  # http://www.railstips.org/blog/archives/2009/03/29/oauth-explained-and-what-it-is-good-for/
-
+  ############################
+  # Testing omniauth/oauth 
+  ############################
   def get_tweets(omniAuth)
     # OmniAuth Hash contains the OAutch access token object
     access_token = omniAuth.extra.access_token
@@ -11,5 +11,34 @@ module TwitterHelper
     # https://dev.twitter.com/rest/reference/get/statuses/user_timeline
     tweets = access_token.get("/1.1/statuses/user_timeline.json?screen_name=#{omniAuth.extra.raw_info.screen_name}&count=200&exclude_replies=true&include_rts=false").body
   end
+
+  # Create an OAuth Consumer object
+  def build_OAuth_consumer
+    OAuth::Consumer.new(ENV['TWITTER_CONSUMER_KEY'], ENV['TWITTER_CONSUMER_SECRET'], {site: 'https://api.twitter.com'})
+  end
+
+  # Create an OAuth Access Token object
+  def create_access_token(token, secret)
+    consumer = build_OAuth_consumer
+    OAuth::AccessToken.new(consumer, token, secret)
+  end
+
+  #####################
+  # Worker related methods
+  ######################
+  def build_hash(omniauth_hash)
+    user_hash               = Hash.new()
+
+    user_hash[:uid]         = omniauth_hash.uid
+    user_hash[:screen_name] = omniauth_hash.info.nickname
+    user_hash[:location]    = omniauth_hash.info.location
+    user_hash[:token]       = omniauth_hash.credentials.token
+    user_hash[:secret]      = omniauth_hash.credentials.secret
+    user_hash[:name]        = omniauth_hash.info.name
+    user_hash[:time_zone]   = omniauth_hash.extra.time_zone
+
+    user_hash
+  end
+
 
 end
