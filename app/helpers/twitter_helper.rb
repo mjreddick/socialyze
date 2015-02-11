8b477922615b0749bf144773440b5e4532741799
jakoko <aerisgurlie777@gmail.com>
2015-02-10 19:30:26 -0800

    updated query

--- app/helpers/twitter_helper.rb
+++ app/helpers/twitter_helper.rb
@@ -9,7 +9,7 @@ module TwitterHelper
 
     # Use the access token to make the api call
     # https://dev.twitter.com/rest/reference/get/statuses/user_timeline
-    tweets = access_token.get("/1.1/statuses/user_timeline.json?screen_name=#{omniAuth.extra.raw_info.screen_name}").body
+    tweets = access_token.get("/1.1/statuses/user_timeline.json?screen_name=#{omniAuth.extra.raw_info.screen_name}&count=200&exclude_replies=true&include_rts=false").body
   end
 
 end
