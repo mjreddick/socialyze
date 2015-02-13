4ab11c673fe630c358d0c26822445d2d89fff86e
Long Huynh <longh957@gmail.com>
2015-02-12 23:51:24 -0800

    add ng-resoure and pulling tweets from our api

--- app/assets/javascripts/twitter_controller.js
+++ app/assets/javascripts/twitter_controller.js
@@ -2,9 +2,21 @@ angular
   .module("socialyze")
   .controller("TwitterController", TwitterController);
 
+  TwitterController.$inject = ['$resource'];
+
 
 // Angular Twitter Controller to control twitter angular template
-  function TwitterController(){
+  function TwitterController($resource){
     var self = this;
+
+
+    var Tweets = $resource('http://localhost:3000/api/tweets',
+        {update: {method: 'PUT'}} );
+
     self.test = "Im in the ng-twitter controller"
+    self.tweetList = getTweets();
+
+    function getTweets() {
+      return Tweets.get();
+    };
   }
