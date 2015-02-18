1de3338990341e109ec16bef1b0bb489e7a66912
Long Huynh <longh957@gmail.com>
2015-02-17 23:23:11 -0800

    add login and logout for token auth

--- app/assets/javascripts/controllers/authentication_controller.js
+++ app/assets/javascripts/controllers/authentication_controller.js
@@ -11,6 +11,7 @@ AuthenticationController.$inject = ['$http'];
     self.password;
     self.login = login;
     self.isAuthenticated = isAuthenticated();
+    self.logout = logout;
 
     function login(){
       $http.post('/api/authenticate', {
@@ -37,5 +38,9 @@ AuthenticationController.$inject = ['$http'];
       return window.sessionStorage.access_token ? true : false;
     } // End isAuthenticated function
 
+    function logout(){
+      window.sessionStorage.clear();
+      self.isAuthenticated = isAuthenticated();
+    }
 
   } // End Auth function
