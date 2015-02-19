5947a480e7127bcee4d9654c4f1bc030c1fd5adf
Matthew Reddick <matthewjreddick@gmail.com>
2015-02-18 21:19:39 -0800

    fix twitter worker

--- app/worker/twitter_build_user_data.rb
+++ app/worker/twitter_build_user_data.rb
@@ -9,11 +9,25 @@ class TwitterBuildUserData
  
     user = User.find(user_id) 
 
-    ##### Add user's twitter account to twitter_account table #####
-    account = user.build_twitter_account(twitter_uid: info_hash["uid"],token: info_hash["token"],secret: info_hash["secret"],username: info_hash["screen_name"],time_zone: info_hash["time_zone"])
+    # Check if a twitter account with the user's uid already exists
 
+    account = TwitterAccount.find_by(twitter_uid: info_hash["uid"])
+
+    if account
+      # the user's twitter account already exists on our site
+      # so add the releation and all other data to the user's account
+      account.user = user
+      account.token = info_hash["token"]
+      account.secret = info_hash["secret"]
+      account.time_zone = info_hash["time_zone"]
+    else
+      # The user does not already have a twitter account saved in our database so make one
+      account = user.build_twitter_account(twitter_uid: info_hash["uid"],token: info_hash["token"],secret: info_hash["secret"],username: info_hash["screen_name"],time_zone: info_hash["time_zone"])
+      
+      # user.save!
+    end
     account.save!
-    user.save!
+
 
     #### Add tweets to tweets table #####
     # get the response body with a user's tweets
@@ -31,9 +45,8 @@ class TwitterBuildUserData
         # if the save succeeded then the tweet is unique
         # break the tweet up into words and save those words
         words = split_tweet_into_words(item["text"])
-        # puts tweet_words.inspect
+
         words.each do |word|
-          puts word
           # save each word unless it is a stop word
           user.twitter_account.tweet_words.create(word: word) unless StopWord.find_by(word: word)
         end
@@ -69,31 +82,49 @@ class TwitterBuildUserData
       unless parsed_follower_tweets.blank? || parsed_follower_tweets.class != Array
         # Use the data from the first tweet to build the follower's twitter account
         first_tweet = parsed_follower_tweets.first
-        # Create twitter_account and relationships
-        follower_account = user.twitter_account.followers.create!(twitter_uid: first_tweet["user"]["id_str"], username: first_tweet["user"]["screen_name"])
 
-        puts "Created follower account"
+        # Check if follower's account already exists in our db
+        follower_account = TwitterAccount.find_by(twitter_uid: first_tweet["user"]["id_str"])
 
         if follower_account
-          # if the follower's twitter account is saved then walk through all their tweets
-          parsed_follower_tweets.each do |item|
-            tweet = follower_account.tweets.build
-            tweet.tweet_text = item["text"]
-            tweet.twitter_tweet_id = item["id_str"]
-
-            if tweet.save
-              ##### Build words table #####
-              # if the save succeeded then the tweet is unique
-              # break the tweet up into words and save those words
-              words = split_tweet_into_words(item["text"])
-              words.each do |word|
-                # save each word unless it is a stop word
-                follower_account.tweet_words.create(word: word) unless StopWord.find_by(word: word)
-              end
-            end
+          # the follower's twitter account already exists
+          # assume we already have tweets for this account so don't get them
+          # check if the connection between user and follower already exists
+          connection = TwitterFollowerRelationship.find_by(followee: user.twitter_account, follower: follower_account)
+          unless connection
+            # if the connection doesn't exist then create it
+            TwitterFollowerRelationship.create(followee: user.twitter_account, follower: follower_account)
           end
-        end
-      end
+          
+        else
+          # the follower's twitter account does not exist
+
+          # Create twitter_account and relationships
+          follower_account = user.twitter_account.followers.create!(twitter_uid: first_tweet["user"]["id_str"], username: first_tweet["user"]["screen_name"])
+
+          puts "Created follower account"
+
+          if follower_account
+            # if the follower's twitter account is saved then walk through all their tweets
+            parsed_follower_tweets.each do |item|
+              tweet = follower_account.tweets.build
+              tweet.tweet_text = item["text"]
+              tweet.twitter_tweet_id = item["id_str"]
+
+              if tweet.save
+                ##### Build words table #####
+                # if the save succeeded then the tweet is unique
+                # break the tweet up into words and save those words
+                words = split_tweet_into_words(item["text"])
+                words.each do |word|
+                  # save each word unless it is a stop word
+                  follower_account.tweet_words.create(word: word) unless StopWord.find_by(word: word)
+                end # loop through follower's tweet's words
+              end # if the tweet saved
+            end # looping through a followers tweets
+          end # if the follower's account saved
+        end # if/else follower already existed
+      end # unless to check if the returned follower data is good
     end # loop through followers
   end # perform
 end # class
