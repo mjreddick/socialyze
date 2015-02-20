5a97997d96747bb6ae6a3b071063160672199d7e
Matthew Reddick <matthewjreddick@gmail.com>
2015-02-19 21:25:15 -0800

    move all results helper functions to twitter_result_helper, change output format for a few functions

--- app/helpers/twitter_result_helper.rb
+++ app/helpers/twitter_result_helper.rb
@@ -1,4 +1,5 @@
 module TwitterResultHelper
+  # Functions to do the twitter analysis to be put into the twitter_results table
 
   # Get user's number of followers
   def num_followers(user)
@@ -50,5 +51,62 @@ module TwitterResultHelper
 
     char_per_tweet_hsh
   end # end of char_per_tweet
+
+  def avg_tweet_length(user)
+    user.twitter_account.tweets.average("character_count").to_f.round(2)
+  end
+
+  def calculate_early_bird(user)
+    # Count total number of user's tweets
+    total_tweets = user.twitter_account.tweets.count
+
+    return nil if total_tweets.zero?
+
+    total_early_tweets = user.twitter_account.tweets.where(hour: 6..18).count
+
+    # Percentage of total tweets that were early tweets
+    percentage = (total_early_tweets.to_f) / (total_tweets.to_f) * 100
+
+    # user.twitter_result.save!
+    percentage >= 50
+  end # end of calculate_early_bird
+
+  # Get top 10 most used words by the current user
+  # Returns a hash where the key is a word and the value is the number of times that word is used
+  def get_users_most_used_words(user)
+    word_hash = TweetWord.where(twitter_account_id: user.twitter_account.id).group("word").order("count_word DESC").limit(10).count("word")
+    hash_to_word_hash_array(word_hash)
+  end
+
+  # Get the 10 least used words by the current user
+  # Returns a hash where the key is a word and the value is the number of times that word is used
+  def get_users_least_used_words(user)
+    word_hash = TweetWord.where(twitter_account_id: user.twitter_account.id).group("word").order("count_word ASC").limit(10).count("word")
+    hash_to_word_hash_array(word_hash)
+  end
+
+  def get_followers_avg_tweet_length(user)
+    Tweet.joins("INNER JOIN twitter_follower_relationships ON tweets.twitter_account_id = twitter_follower_relationships.follower_id AND twitter_follower_relationships.followee_id = #{user.twitter_account.id}").average("character_count").to_f.round(2)
+  end
+
+  def get_followers_most_used_words(user)
+    word_hash = TweetWord.joins("INNER JOIN twitter_follower_relationships ON tweet_words.twitter_account_id = twitter_follower_relationships.follower_id AND twitter_follower_relationships.followee_id = #{user.twitter_account.id}").group("word").order("count_word DESC").limit(10).count("word")
+    hash_to_word_hash_array(word_hash)
+  end
+
+  def get_followers_least_used_words(user)
+    word_hash = TweetWord.joins("INNER JOIN twitter_follower_relationships ON tweet_words.twitter_account_id = twitter_follower_relationships.follower_id AND twitter_follower_relationships.followee_id = #{user.twitter_account.id}").group("word").order("count_word ASC").limit(10).count("word")
+    hash_to_word_hash_array(word_hash)
+  end
+
+  def hash_to_word_hash_array(hash)
+    # Convert {"apple" => 5, "bear" => 3}
+    # To [{word: "apple", value: 5}, {word: "bear", value: 3}]
+    result = []
+    hash.each do |key, value|
+      result << {word: key, value: value}
+    end
+    result
+  end
 
 end # end Module
